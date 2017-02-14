e074d9b3339024d6052cbc96962f2555596d4dac
Birhanu Hailemariam <birhanuh@gmail.com>
2017-02-14 12:46:45 +0200

    Add server2.js to git ingnore. Add index.jsx. Install react-dom module.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // Get Homepage
 router.get('/', function(req, res) {
-  res.render('landing.jade');
+  res.render('index', {});
 });
 
 router.get('/dashboard', ensureAuthenticated, function(req, res) {
---
+++ app/views/index.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var IndexComponent = React.createClass({
+  render: function (argument) {
+    return (
+        <div>
+        <h1>This is react</h1>
+        </div>
+      )
+  }
+});
+
+module.exports = IndexComponent;
