654c62ace79cc4123838e72bee67a134b76d34a0
Birhanu Hailemariam <birhanuh@gmail.com>
2017-03-20 16:46:17 +0200

    Remove app, test directories. Remove .bablerc, server.js, npmignore files.

--- app/src/components/layout/index.js
+++
@@ -1,9 +0,0 @@
-import Home from './Home'
-import Landing from './Landing'
-import Dashboard from './Dashboard'
-
-export {
-  Home,
-  Landing,
-  Dashboard
-}
--- app/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-import APIManager from './APIManager'
-import Validation from './Validation'
-
-export {
-  APIManager,
-  Validation
-}
