21a76cbba9d3c0add85b7e8d4eba2dc5a38e756b
Birhanu Hailemariam <birhanuh@gmail.com>
2017-07-31 18:58:29 +0300

    Implement post.save vatNumber validator schema middleware function.

--- server/src/models/Customer.js
+++ server/src/models/Customer.js
@@ -8,7 +8,7 @@ let customerSchema = new mongoose.Schema({
     region: { type: String, required: [true, "Region is required."] },
     country: { type: String, required: [true, "Country is required."] }
   },
-  vatNumber: { type: String, required: [true, "Vat number is required."] },
+  vatNumber: { type: String, required: [true, "Vat number is required."], unique: true },
   contact: {
     phoneNumber: { type: String, match: [/\d{6,14}/, "Wrong Phone number fromat."], validate: { validator: contactValidator, message: 'Either Phone number or email should be filled.'} },
     email: { type: String, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Wrong Email fromat."], validate: { validator: contactValidator, message: 'Either Phone number or email should be filled.'} }
@@ -23,5 +23,20 @@ function contactValidator() {
     return true
   }
 }
+
+customerSchema.post('save', function(error, doc, next) {
+  if (error.name === 'MongoError' && error.code === 11000) {
+    let message = {
+      errors: {
+        vatNumber: {
+          message: 'Vat number must be unique.'
+        } 
+      }
+    }
+    next(message)
+  } else {
+    next(error)
+  }
+})
 
 let Customer = module.exports = mongoose.model('Customer', customerSchema)
