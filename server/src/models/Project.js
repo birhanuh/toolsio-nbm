2fd779f064994a4c2143efe13e7a3c2e1bc7e4d5
Birhanu Hailemariam <birhanuh@gmail.com>
2017-10-11 23:54:24 +0300

    Remove envirnomnent config property.

--- server/src/models/Project.js
+++ server/src/models/Project.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 let projectSchema = new mongoose.Schema({
   name: { type: String, required: [true, "Name is required."] },
   deadline: { type: Date, required: [true, "Deadline is required."] },
-  customer: { type: mongoose.Schema.Types.ObjectId, ref: "Customer", required: [true, "Customer is required."] },
+  customer: { type: mongoose.Schema.Types.ObjectId, ref: "customer", required: [true, "Customer is required."] },
   status: { type: String, required: [true, "Status is required."] },
   description: { type: String, default: '' },
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "task" }],
