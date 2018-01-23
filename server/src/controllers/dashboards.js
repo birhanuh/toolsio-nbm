19632ef0ae8adb8c507791e3af1eb8e1cc9cb084
Birhanu Hailemariam <birhanuh@gmail.com>
2018-01-23 19:02:12 +0200

    Implement thirtyDaysEarlierDate calculated variable.

--- server/src/controllers/dashboards.js
+++ server/src/controllers/dashboards.js
@@ -4,6 +4,10 @@ import Sale from '../models/sale'
 import User from '../models/user'
 import Customer from '../models/customer'
 
+let today = new Date()
+let thirtyDaysEarlierDateLong = new Date().setDate(today.getDate()-30)
+let thirtyDaysEarlierDate = new Date(thirtyDaysEarlierDateLong)
+
 export default {
   find: (req, callback) => {
 
@@ -29,33 +33,44 @@ export default {
 
      // Invoices
     if (type === 'invoices') {
-     //  Project.aggregate( [
-     //   {
-     //     $group:
-     //       {
-     //          _id: "$status",  
-     //          count: { $sum: 1 }
-     //       }
-     //   }
-     // ], function (err, result) {
-     //      if (err) {
-     //          console.log(err);
-     //          return;
-     //      }
-     //      console.log(result);
-     //    })
-     //    return 
-     //  }
-
-      Invoice.aggregate( [ {$project: {_id: 0, status: 1, week: { $week: "$createdAt" } }} ], function (err, result) {
-          if (err) {
-              callback(err, null)
-              return;
+      //  Project.aggregate( [
+      //   {
+      //     $group:
+      //       {
+      //          _id: "$status",  
+      //          count: { $sum: 1 }
+      //       }
+      //   }
+      // ], function (err, result) {
+      //      if (err) {
+      //          console.log(err);
+      //          return;
+      //      }
+      //      console.log(result);
+      //    })
+      //    return 
+      //  }
+      
+      Project.aggregate( [
+        {$match: {
+          createdAt: {
+            $gte: thirtyDaysEarlierDate
           }
-          console.log('result: ', result)
-          callback(null, result)
-          return 
-        })
+        }},
+        {$project: 
+          {_id: 0, 
+            status: 1, 
+            week: { $week: "$createdAt" } 
+          }
+        }], function (err, results) {
+        if (err) {
+          callback(err, null)
+          return
+        }
+        console.log('result: ', results)
+        callback(null, results)
+        return 
+      })
       }
 
     // Tasks
