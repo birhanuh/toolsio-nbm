a3f925fdb92a337ba606c902ffe11cbc37265d12
Birhanu Hailemariam <birhanuh@gmail.com>
2018-07-10 10:59:42 +0300

    Install dotenv module. Add CLOUDINARY env variables.

--- client/config/webpack.dev.js
+++ client/config/webpack.dev.js
@@ -3,6 +3,9 @@ const path = require('path')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
+// env
+require('dotenv').config()
+
 module.exports = merge(common, {
   devtool: 'cheap-module-source-map',
   devServer: {
@@ -25,8 +28,11 @@ module.exports = merge(common, {
       'process.env.CLIENT_URL': JSON.stringify('lvh.me:3000'),
       'process.env.CLIENT_PROTOCOL': JSON.stringify('http://'),
       'process.env.SERVER_URL': JSON.stringify('localhost:8080'),
-      'process.env.JWTSECRET1': 'somesecretkeyforjsonwebtoken',
-      'process.env.JWTSECRET2': 'somesecretkeyforjsonwebtokentwo'
+      'process.env.JWTSECRET1': JSON.stringify(process.env.JWTSECRET1),
+      'process.env.JWTSECRET2': JSON.stringify(process.env.JWTSECRET2),
+      'process.env.CLOUDINARY_API_URL': JSON.stringify(process.env.CLOUDINARY_API_URL),
+      'process.env.CLOUDINARY_PRESET_AVATARS': JSON.stringify(process.env.CLOUDINARY_PRESET_AVATARS),
+      'process.env.CLOUDINARY_PRESET_LOGOS': JSON.stringify(process.env.CLOUDINARY_PRESET_LOGOS)
     })
   ]
 
--- client/config/webpack.prod.js
+++ client/config/webpack.prod.js
@@ -3,6 +3,9 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
+// env
+require('dotenv').config()
+
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
@@ -10,14 +13,16 @@ module.exports = merge(common, {
       sourceMap: true
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      //'process.env.CLIENT_URL': JSON.stringify('toolsio.com'),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      //'process.env.CLIENT_URL': JSON.stringify(process.env.CLIENT_URL),
       'process.env.CLIENT_URL': JSON.stringify('lvh.me:3000'),
-      //'process.env.CLIENT_PROTOCOL': JSON.stringify('https://'),
+      //'process.env.CLIENT_PROTOCOL': JSON.stringify(process.env.CLIENT_PROTOCOL),
       'process.env.CLIENT_PROTOCOL': JSON.stringify('http://'),
       'process.env.SERVER_URL': JSON.stringify('localhost:8080'),
-      'process.env.JWTSECRET1': 'somesecretkeyforjsonwebtoken',
-      'process.env.JWTSECRET2': 'somesecretkeyforjsonwebtokentwo'
+      'process.env.JWTSECRET1': JSON.stringify(process.env.JWTSECRET1),
+      'process.env.JWTSECRET2': JSON.stringify(process.env.JWTSECRET2),
+      'process.env.CLOUDINARY_API_URL': JSON.stringify(process.env.CLOUDINARY_API_URL),
+      'process.env.CLOUDINARY_PRESET': JSON.stringify(process.env.CLOUDINARY_PRESET)
     })
   ]
 
